378040584700e9bb5ce1ed9121785499072fa2ca
hargo-xyz <kharismahargo@gmail.com>
2025-03-02 11:37:55 +0700

    part1

---
+++ quiz/part1/soal3.js
@@ -0,0 +1,20 @@
+function cariMedian(arr) {
+    // you can only write your code here!
+    arr.sort((a, b) => a - b)
+    let median;
+    if(arr.length % 2 == 0){
+        let mid1 = arr[arr.length / 2 - 1]
+        let mid2 = arr[arr.length / 2]
+        median = (mid1 + mid2) / 2
+    }else{
+        median = arr[Math.floor(arr.length / 2)]
+    }
+    return median
+}
+
+// TEST CASES
+console.log(cariMedian([1, 2, 3, 4, 5])); // 3
+console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
+console.log(cariMedian([3, 4, 7, 6, 10])); // 6
+console.log(cariMedian([1, 3, 3])); // 3
+console.log(cariMedian([7, 7, 8, 8])); // 7.5
